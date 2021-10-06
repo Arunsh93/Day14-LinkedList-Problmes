31a1093749fe1ee210442ebe837b5b2f25934015
Arunsh93 <Arunhosur93@gmail.com>
2021-10-06 16:28:58 +0530

    Create a Simple LinkedList

---
+++ LinkedListProject/LinkedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListProject
+{
+    class LinkedList
+    {
+        public Node head;
+
+        public void Add(int value)
+        {
+            Node node = new Node(value);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Added to the List", node.data);
+        }
+    }
+}
---
+++ LinkedListProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Problems");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+
+        }
+    }
+}
